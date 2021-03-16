11cefcd91fe03437f19031a7f6437135c7fc7795
pgueugno <pgueugno@student.42.fr>
2021-03-16 13:08:14 +0100

    openssl bug with nginx refusing to start to correct

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM debian:buster
 MAINTAINER pgueugno <pgueugno@student.42.fr>
-RUN apt-get update && apt-get install -y nginx
+RUN apt-get update && apt-get install -y nginx && apt-get install -y openssl
+RUN openssl req -newkey rsa:4096 -sha256 -x509 -days 365 -nodes -out /etc/ssl/certs/cert.pem -keyout /etc/ssl/private/key.pem -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=pgueugno/CN=localhost"
 RUN mkdir -p /var/www/projet/html
 RUN chmod 755 /var/www/projet
 COPY projet.html /var/www/projet/html/
-COPY nginx.conf /etc/nginx/
+#COPY nginx.conf /etc/nginx/
+#RUN	rm /etc/nginx/nginx.conf
 COPY default /etc/nginx/sites-available/
 #RUN ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 #RUN service nginx start
--- start.sh
+++ start.sh
@@ -13,16 +13,16 @@ sudo chmod 666 /var/run/docker.sock
 #sudo usermod -aG docker ${USER}
 #newgrp docker
 #su -s ${USER}
-docker rm -f $(docker ps -qa)
-docker rmi $(docker images -qa)
+sudo docker rm -f $(docker ps -qa)
+sudo docker rmi $(docker images -qa)
 #docker images purge
-docker build -t ft_server .
+sudo docker build -t ft_server .
 #sudo cp projet.html /var/www/projet/html/projet.html\
 #&& sudo cp nginx.conf /etc/nginx/\
 #&& sudo cp server_config /etc/nginx/sites-available/\
 #&& sudo ln -s /etc/nginx/sites-available/server_config /etc/nginx/sites-enabled/
 #&& sudo systemctl reload nginx.service
-docker run -td -p 80:80 --name=ft_server ft_server
+docker run -tid -p 80:80 -p 443:443 --name=ft_server ft_server
 docker exec -it ft_server service nginx start
 #docker exec -it ft_server service nginx stop
 #docker stop ft_server
