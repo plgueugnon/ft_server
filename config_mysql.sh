720a598363073aa44edc72cb6fea6b72b4120278
pgueugno <pgueugno@student.42.fr>
2021-03-23 12:32:45 +0100

    wordress nearly OK - autoindex not yet managed

--- config_mysql.sh
+++ config_mysql.sh
@@ -1,3 +1,5 @@
 echo "create database dbprojet default character set utf8 collate utf8_unicode_ci;" | mysql -u root
 echo "grant all on dbprojet.* to 'yo'@'localhost' identified by 'yopwd';" | mysql -u root
 echo "flush privileges;" | mysql -u root
+wp core config --dbname=dbprojet --dbuser=yo --dbpass=yopwd --dbhost=localhost --path=/var/www/projet --allow-root
+wp core install --url="https://localhost/wordpress/" --title="PROOOOJET" --admin_user="yo" --admin_password="yopwd" --admin_email="yo@yo.com" --path=/var/www/projet/ --allow-root
--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash/
 
 # OPTION 1
+echo "Please kill nginx service before starting"
 sudo systemctl stop nginx
+echo "thank you!"
 #&& sudo mkdir -p /var/www/projet/html
 
 #sudo chmod 666 /var/run/docker.sock
@@ -13,10 +15,12 @@ sudo systemctl stop nginx
 #sudo usermod -aG docker ${USER}
 #newgrp docker
 #su -s ${USER}
+echo "killing previous running containers"
 sudo docker kill $(docker ps -qa)
 #sudo docker rmi $(docker images -qa)
 #docker images purge
 echo "Starting to build\n"
+touch log.txt
 sudo docker build -t ft_server . >> log.txt
 echo "Build finished\n"
 #sudo cp projet.html /var/www/projet/html/projet.html\
@@ -24,6 +28,7 @@ echo "Build finished\n"
 #&& sudo cp server_config /etc/nginx/sites-available/\
 #&& sudo ln -s /etc/nginx/sites-available/server_config /etc/nginx/sites-enabled/
 #&& sudo systemctl reload nginx.service
+echo "launching container"
 docker run -d -p 80:80 -p 443:443 ft_server
 # docker exec -it ft_server service nginx start
 #docker exec -it ft_server service nginx stop
